ef7a078db3119ad7c8057774fdc76493f126ac43
HotBaconWasTaken <116873925+nicola-stroffolino-scuola@users.noreply.github.com>
2023-04-13 13:37:47 +0200

    Initial commit

---
+++ FileDataManagement/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FileDataManagement;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ FileDataManagement/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FileDataManagement;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
